aab11a01dcc246409b5c3bc5dcf592192ff92326
Dean Rickles <dean@rickles.co.uk>
2025-03-11 22:58:23 +0000

    Altered for my own taste.

--- config/constants.h
+++ config/constants.h
@@ -7,16 +7,11 @@
 // Layers
 // Must match order in which they are defined
 #define COL 0
-#define CAC 1
-#define QWE 2
-#define QAC 3
-#define ERG 4
-#define EAC 5
-#define SYM 6
-#define NAV 7
-#define NUM 8
-#define MOU 9
-#define GAM 10
+#define SYM 1
+#define NAV 2
+#define FNC 3
+#define NUM 4
+#define MOU 5
 
 // Modifier keys
 #define GL LGUI
