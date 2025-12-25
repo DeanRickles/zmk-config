d6ce57c6628ba758583844ec03e84b68849429e0
Dean Rickles <dean@rickles.co.uk>
2025-12-25 20:24:32 +0000

    Dont like how hold-tap works for me.

--- config/constants.h
+++ config/constants.h
@@ -1,11 +1,15 @@
 #pragma once
 
-// Keys
+/* Keys */
 #define ___ &trans
 #define XXX &none
 
-// Layers
-// Must match order in which they are defined
+/* OS */
+#define WINDOWS 0
+#define LINUX   1
+#define MAC_OS  2
+
+/*  Layers -Must match order in which they are defined */
 #define COL 0
 #define MOU 1
 #define NUM 2
@@ -13,7 +17,7 @@
 #define NAV 4
 #define FNC 5
 
-// Modifier keys
+/* Modifier keys */
 #define GL LGUI
 #define CL LCTRL
 #define AL LALT
@@ -23,14 +27,25 @@
 #define AR RALT
 #define SR RSHFT
 
-// Bluetooth keys
+/* Bluetooth keys */
 #define BTS0  &bt BT_SEL 0
 #define BTS1  &bt BT_SEL 1
 #define BTS2  &bt BT_SEL 2
 #define BTS3  &bt BT_SEL 3
 #define BTCLR &bt BT_CLR
 
-// OS
-#define WINDOWS 0
-#define LINUX 1
-#define MAC_OS 2
+/* Custom keys */
+#define BL        &bootloader
+#define SL_SPACE  &mt SL SPACE
+#define NUM_ENTER &lt NUM ENTER
+#define MOU_BSPC  &to_kp MOU BSPC
+
+/*Custom keys - volume */
+#define VOL_U     &kp C_VOL_UP
+#define VOL_M     &kp C_MUTE
+#define VOL_D     &kp C_VOL_DN
+/*Custom keys - brightness */
+#define BRI_U     &kp C_BRI_UP
+#define BRI_D     &kp C_BRI_DN
+
+
